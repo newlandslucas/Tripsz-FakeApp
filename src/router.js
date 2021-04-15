94b6791ee0b29fcc9dd87fe5e8af462c225c2dff
newlandslucas <lucasnewlands.m@gmail.com>
2021-04-14 23:01:44 -0300

    Routes Configured

---
+++ src/router.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { Feather } from '@expo-vector-items';
+
+import Home from './pages/Home';
+import Detail from './pages/Detail';
+
+const Stack = createStackNavigator();
+
+
+function Routes() {
+    return(
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="home" component={Home}/>
+                <Stack.Screen name="Detail" component={Detail}/>
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
+
+export default Routes;
